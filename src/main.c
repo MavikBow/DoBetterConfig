c89bdbfcf28583f0461d120d1ffecca2d491c406
MavikBow <mrmavikd@gmail.com>
2024-03-08 16:49:48 +0300

    trying out a ListView

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <windows.h>
+#include <commctrl.h>
 #include "patcher.h"
 
 HWND hButton_Menu;
+HWND hWndListView;
+typedef struct PETINFO
+        {
+            TCHAR szKind[10];
+            TCHAR szBreed[50];
+            TCHAR szPrice[20];
+        }PETINFO;
+
+        struct PETINFO rgPetInfo[ ] = 
+        {
+            {TEXT("Dog"),  TEXT("Poodle"),     TEXT("$300.00")},
+            {TEXT("Cat"),  TEXT("Siamese"),    TEXT("$100.00")},
+            {TEXT("Fish"), TEXT("Angel Fish"), TEXT("$10.00")},
+            {TEXT("Bird"), TEXT("Parakeet"),   TEXT("$5.00")},
+            {TEXT("Toad"), TEXT("Woodhouse"),  TEXT("$0.25")},
+        };
+void AddMenus(HWND);
 
 // Is called by the message loop
 
@@ -9,9 +27,17 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch(uMsg)
 	{
+		case WM_NOTIFY:
+			HandleWM_NOTIFY(lParam);
+
+			break;
+
 		case WM_SYSKEYDOWN:
 		case WM_KEYDOWN:
 			printf(" 0x%x\t%d\t%s\n", (int)wParam, (int)wParam, keyName(wParam));
+			break;
+		case WM_CREATE:
+			AddMenus(hWnd);
 			break;
 		case WM_NCCREATE:
 			readInput();
@@ -74,4 +100,79 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 	}
 
 	return 0;
+}
+
+BOOL InsertListViewItems(HWND hWndListView, int cItems)
+{
+    LVITEM lvI;
+
+    // Initialize LVITEM members that are common to all items.
+    lvI.pszText   = LPSTR_TEXTCALLBACK; // Sends an LVN_GETDISPINFO message.
+    lvI.mask      = LVIF_TEXT | LVIF_IMAGE |LVIF_STATE;
+    lvI.stateMask = 0;
+    lvI.iSubItem  = 0;
+    lvI.state     = 0;
+
+    // Initialize LVITEM members that are different for each item.
+    for (int index = 0; index < cItems; index++)
+    {
+        lvI.iItem  = index;
+        lvI.iImage = index;
+    
+        // Insert items into the list.
+        if (ListView_InsertItem(hWndListView, &lvI) == -1)
+            return FALSE;
+    }
+
+    return TRUE;
+}
+void HandleWM_NOTIFY(LPARAM lParam)
+{
+    NMLVDISPINFO* plvdi;
+
+    switch (((LPNMHDR) lParam)->code)
+    {
+        case LVN_GETDISPINFO:
+
+            plvdi = (NMLVDISPINFO*)lParam;
+
+            switch (plvdi->item.iSubItem)
+            {
+                case 0:
+                    plvdi->item.pszText = rgPetInfo[plvdi->item.iItem].szKind;
+                    break;
+                      
+                case 1:
+                    plvdi->item.pszText = rgPetInfo[plvdi->item.iItem].szBreed;
+                    break;
+                
+                case 2:
+                    plvdi->item.pszText = rgPetInfo[plvdi->item.iItem].szPrice;
+                    break;
+                
+                default:
+                    break;
+            }
+
+        break;
+
+    }
+	return;
+}	
+void AddMenus(HWND hWnd)
+{
+	//InitCommonControls();
+	hWndListView = CreateWindowA(WC_LISTVIEW, 
+                                     "",
+                                     WS_CHILD | WS_VISIBLE | LVS_LIST,
+                                     100, 100,
+                                     300,
+                                     300,
+                                     hWnd,
+									 NULL,
+                                     //(HMENU)IDM_CODE_SAMPLES,
+                                     //g_hInst,
+									 NULL,
+                                     NULL); 
+	InsertListViewItems(hWndListView, 2);
 }
